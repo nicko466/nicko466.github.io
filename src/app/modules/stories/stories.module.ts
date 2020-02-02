dd21af2ed49b41ee03e0d69096a39d4a92bf614c
nicko466 <nicko466@gmail.com>
2020-02-02 16:27:58 +0800

    fix(scroll): fix mobile scroll on text

--- src/app/modules/stories/stories.module.ts
+++ src/app/modules/stories/stories.module.ts
@@ -1,17 +1,38 @@
-import {NgModule} from '@angular/core';
+import {Injectable, NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {StoriesComponent} from './components/stories/stories.component';
 import {StoriesRoutingModule} from './stories-routing.module';
 import {MatTooltipModule} from '@angular/material/tooltip';
+
+import 'hammerjs';
+
+import {HAMMER_GESTURE_CONFIG, HammerGestureConfig} from '@angular/platform-browser';
+import {HammerInstance} from '@angular/material/core';
+
+declare var Hammer: any;
+
+@Injectable()
+export class MyHammerConfig extends HammerGestureConfig {
+    buildHammer(element: HTMLElement): HammerInstance {
+        return new Hammer(element, {
+            touchAction: 'auto'
+        });
+    }
+}
 
 @NgModule({
     declarations: [StoriesComponent],
     imports: [
         MatTooltipModule,
         CommonModule,
-        StoriesRoutingModule
+        StoriesRoutingModule,
     ],
-    providers: []
+    providers: [
+        {
+            provide: HAMMER_GESTURE_CONFIG,
+            useClass: MyHammerConfig
+        },
+    ]
 })
 export class StoriesModule {
 }
