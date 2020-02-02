42f46d4068724e879871d56cc54d1c3834199efa
nicko466 <nicko466@gmail.com>
2020-02-02 16:13:08 +0800

    Revert "feat(material): update major 8 to 9"
    This reverts commit e4386e9a9e8f512608b8c9b34b45920aab4e2c73.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,14 +6,14 @@ import {AppComponent} from './app.component';
 import {SongComponent} from './shared/components/song/song.component';
 import {RepoService} from '../services/repo.service';
 import {HttpClientModule} from '@angular/common/http';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {PageNotFoundComponent} from './shared/components/page-not-found/page-not-found.component';
 import {SongsComponent} from './shared/components/songs/songs.component';
 import {MatButtonModule} from '@angular/material/button';
-import {MatIconModule} from '@angular/material/icon';
-import {MatListModule} from '@angular/material/list';
-import {MatSidenavModule} from '@angular/material/sidenav';
 import {MatToolbarModule} from '@angular/material/toolbar';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatIconModule} from '@angular/material/icon';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatListModule} from '@angular/material/list';
 
 @NgModule({
     declarations: [
