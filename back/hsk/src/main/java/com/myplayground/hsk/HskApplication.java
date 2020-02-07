8e37317875758714cbc387b8d7a82f7467555589
nicko466 <nicko466@gmail.com>
2020-02-07 13:59:34 +0800

    feat(split): move to back/front folder

---
+++ back/hsk/src/main/java/com/myplayground/hsk/HskApplication.java
@@ -0,0 +1,13 @@
+package com.myplayground.hsk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HskApplication.class, args);
+	}
+
+}
---
+++ back/hsk/src/test/java/com/myplayground/hsk/HskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myplayground.hsk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
