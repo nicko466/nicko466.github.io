f2c95808ce76bafaff8ff9796a10fc23be9f2769
nicko466 <nicko466@gmail.com>
2020-03-28 14:21:28 +0800

    feat(covid): increase width graph

--- front/src/app/modules/covid19/components/dashboard/dashboard.component.ts
+++ front/src/app/modules/covid19/components/dashboard/dashboard.component.ts
@@ -104,7 +104,7 @@ export class DashboardComponent implements OnInit, AfterViewInit, OnDestroy {
     private initChart(charHtmlName: string): any {
         const chart = am4core.create(charHtmlName, am4charts.XYChart);
 
-        chart.paddingRight = 20;
+        chart.paddingRight = 0;
         chart.data = [];
 
         const dateAxis = chart.xAxes.push(new am4charts.DateAxis());
